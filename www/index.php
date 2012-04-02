0c1414e8cd56b90f60b271d8e0481eba200b6072
jongejans <jongejans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-02 07:46:11 +0000

    added link to BaSTa
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipmpack@517 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -176,6 +176,8 @@ row represents one measurement in the population:</p>
 					<p>A list of all known IPM papers can be downloaded here: <a href="IPMpublications.xlsx">IPMpublications</a>.
 					<p><br><br><br></p>
 					<p>Photo credits: <br><i>Xanthoparmelia</i> lichens by Ann Pringle<br>crocs by Owen Jones<br><i>Succisa pratensis</i> by Lidewij Keser
+					<p>Webdesign credits: <br>
+					Fernando Colchero of <a href="http://basta.r-forge.r-project.org/" rel="nofollow" style="color:#84002E;font-family:verdana" target="_blank">BaSTa</a>
                   </td>
                 </tr>
               </tbody>
